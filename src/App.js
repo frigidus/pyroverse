816ac262e3d582da7caa9aa5ca29ba0819993f47
frigidus <139004092+frigidus@users.noreply.github.com>
2023-08-19 14:18:13 -0400

    add

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
       <header className="App-header">
         <h1>welcome to pyroverse.net</h1>
         <p>you found my dark corner on the internet! i dont know what to put on here tho</p>
-        <h2>random news</h2>
-        <p>8 - 18 - 2023: im making a 3d game so that should come on here soon yay</p>
+        <h2>game</h2>
+        <p>this is a random game i ported to this website for absolutely no reason</p>
+        <embed src="https://basket-random.frigidus5.repl.co/" height="500px" width="500px"></embed>
+        <p>contact: pyroverse.net@gmail.com because im not getting custom email for 30 bucks</p>
       </header>
     </div>
   );
