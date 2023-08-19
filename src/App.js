b1e29b5989eb8b0879881a0d1cc78bd94239d2e5
frigidus <139004092+frigidus@users.noreply.github.com>
2023-08-19 14:23:48 -0400

    add a lot more

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>you found my dark corner on the internet! i dont know what to put on here tho</p>
         <h2>my favorite game</h2>
         <p>this is my favorite game so i ported to this website for absolutely no reason</p>
-        <embed src="https://basket-random.frigidus5.repl.co/" height="500px" width="500px"></embed>
+        <iframe src="https://basket-random.frigidus5.repl.co/" height="500px" width="500px"></iframe><br />
         <a href="https://basket-random.frigidus5.repl.co/">play it fullscreen</a>
         <h2>contact</h2>
         <p>email: pyroverse.net@gmail.com because im not getting custom email for 30 bucks</p>
