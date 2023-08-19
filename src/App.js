b243804c02d560f0b28580c2758643fad5a419e2
frigidus <139004092+frigidus@users.noreply.github.com>
2023-08-18 20:47:06 -0400

    add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <header className="App-header">
         <h1>welcome to pyroverse.net</h1>
         <p>you found my dark corner on the internet! i dont know what to put on here tho</p>
+        <h2>random news</h2>
+        <p>8 - 18 - 2023: im making a 3d game so that should come on here soon yay</p>
       </header>
     </div>
   );
