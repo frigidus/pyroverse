f4277599917c0649a3a64a41223292c69d2c1630
frigidus <ajbrauer2013@gmail.com>
2023-09-01 10:16:00 -0400

    add more games

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
       <header className="App-header">
         <h1>welcome to pyroverse.net</h1>
         <p>you found my dark corner on the internet! i dont know what to put on here tho</p>
-        <h2>my favorite game</h2>
-        <p>this is my favorite game so i ported to this website for absolutely no reason</p>
-        <iframe src="https://basket-random.frigidus5.repl.co/" height="500px" width="500px"></iframe><br />
-        <a href="https://basket-random.frigidus5.repl.co/">play it fullscreen</a>
+        <h1>games</h1>
+        <p>heres some games i like that you can play</p>
+        <a href="https://basket-random.frigidus5.repl.co/">basket random</a>
+        <a href="https://soccer-random.frigidus5.repl.co/">soccer random</a>
         <h2>contact</h2>
         <p>email: pyroverse.net@gmail.com because im not getting custom email for 30 bucks</p>
       </header>
