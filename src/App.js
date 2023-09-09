ef87bbc1f1e8be4de51c5eff066914031513adfa
frigidus <139004092+frigidus@users.noreply.github.com>
2023-09-09 10:48:46 -0400

    minecwaft

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ function App() {
         <p>heres some games i like that you can play</p>
         <a href="https://basket-random.frigidus5.repl.co/">basket random</a><br />
         <a href="https://soccer-random.frigidus5.repl.co/">soccer random</a>
-        <h2>contact</h2>
+        <h1>minecraft mods</h1>
+        <p>i just got into minecraft so heres some terrible mods i made</p>
+        <a href="https://legacy.curseforge.com/minecraft/mc-mods/rainbow-gem">rainbow ore (i know this is the most copied mod ever)</a>
+        <h1>contact</h1>
         <p>email: pyroverse.net@gmail.com because im not getting custom email for 30 bucks</p>
       </header>
     </div>
