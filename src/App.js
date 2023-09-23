34185a173aacb7ded1597c21dc93650f788891c8
frigidus <139004092+frigidus@users.noreply.github.com>
2023-09-23 11:05:38 -0400

    bro

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>welcome to sizzlrs school! just hang out here i guess</p>
         <h1>chatbox</h1>
         <p>chat about whatever yall are doing</p>
-        <iframe src="https://www3.cbox.ws/box/?boxid=3531955&boxtag=w8M9P7" width="100%" height="450" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe>	
+        <iframe src="https://www3.cbox.ws/box/?boxid=3531955&boxtag=w8M9P7" width="450" height="450" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe>	
       </header>
     </div>
   );
