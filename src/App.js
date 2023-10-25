11ca609544039eda95a0b63ff41c1fd4650b9e11
frigidus <139004092+frigidus@users.noreply.github.com>
2023-10-25 16:40:31 -0400

    eagles

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>sizzlrs school</h1>
-        <p>welcome to sizzlrs school! just hang out here i guess</p>
-        <h1>chatbox</h1>
-        <p>chat about whatever yall are doing</p>
-        <iframe src="https://www3.cbox.ws/box/?boxid=3531955&boxtag=w8M9P7" width="450" height="450" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe>	
+        <h1>welcome to the pyroverse</h1>
+        <p>coming soon</p>
       </header>
     </div>
   );
